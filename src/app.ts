fdb13a8a102dcdeba366923e46a0a84ea45feb09
Leandro Costa <lcosta200212@gmail.com>
2024-08-30 14:31:00 -0300

    feat: fastify app and server settings

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,26 @@
+import path from "node:path";
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import formBody from '@fastify/formbody'
-import { uploadRoutes } from "./api/routes/upload-file";
+import fastifyStatic from '@fastify/static'
+import { MeasureRoutes } from "./api/routes/MeasureRoutes";
+import { corsOptions } from "./api/config/corsConfig";
+import { errorHandler } from "./api/middlewares/GlobalExceptionHandler";
 
-const app = fastify()
 
-const corsOptions = {
-  credentials: false,
-  origin: /localhost\:5173/
-}
+const app = fastify({
+  logger: true
+})
+
+app.register(fastifyStatic, {
+  root: path.join(__dirname, "public/uploads"),
+  prefix: "/uploads/"
+})
 
 app.register(cors, corsOptions)
 app.register(formBody)
-app.register(uploadRoutes)
+app.setErrorHandler(errorHandler)
+app.register(MeasureRoutes)
 
 export {
   app
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { app } from "./app"
 
 const PORT = Number(process.env.PORT) || 3333
 
-app.listen({ port: PORT }).then(() => {
+app.listen({ port: PORT, host: "0.0.0.0" }).then(() => {
   console.log(`HTTP server running port ${PORT}`)
 })
